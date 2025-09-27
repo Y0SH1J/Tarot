3437622b5b5896b4434a49e6b576f5855ba9af67
Y0SH1J <yashikajamwal07@gmail.com>
2025-09-27 13:01:19 +0400

    Updated Code

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,33 @@
 // keep track of flipped cards
 let flipCount = 0;
-const maxFlip = 20;
+let maxFlip = 0;
+let selectedCards = [];
+let enterClicked = false;
 
-function flipCard(cardElement) {
+function flipCard(cardElement, cardName) {
     const inner = cardElement.querySelector('.card-inner');
 
     if (!inner.classList.contains('flipped') && flipCount < maxFlip) {
         inner.classList.toggle('flipped');
         flipCount++;
-        return;
+        selectedCards.push(cardName);
     }
+
+    if (flipCount == maxFlip && enterClicked) {
+        const overlay = document.getElementById('overlay');
+        const list = document.getElementById('selected-cards-list');
+        let cleanName = "";
+
+        // Clean the key elements before mapping
+        for (let i in selectedCards){
+            cleanName = selectedCards[i].split("/").pop().replace(".jpg", "");
+            selectedCards[i] = cleanName;
+        }
+
+        list.innerHTML = selectedCards.map(name => `<li>${name}</li>`).join('');
+        overlay.style.display = 'flex';
+    }
+    return;
 }
 
 function shuffleCards(containerSelector){
@@ -26,6 +44,37 @@ function shuffleCards(containerSelector){
     cards.forEach(card => container.appendChild(card));
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-    shuffleCards('.all-cards'); // pass your card container
+document.addEventListener('DOMContentLoaded', () => {
+
+    shuffleCards('.all-cards'); // pass your card container to function for shuffling
+    
+    // Event listener for displaying number of cards to be selected
+    document.getElementById('card-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        maxFlip = parseInt(document.getElementById('num_cards').value);
+        document.getElementById('form-message').textContent = `Select ${maxFlip} cards`;
+        enterClicked = true;
+    });
+
+    // Event listener to reset everything for a new reading
+    document.getElementById("reset-overlay").addEventListener("click", () =>{
+
+        // Flip all the cards back
+        document.querySelectorAll(".card-inner").forEach(inner => {
+            inner.classList.remove("flipped");
+        });
+
+        selectedCards = []; // empty list of selected cards
+
+        // Hide overlay
+        document.getElementById("overlay").style.display = "none";
+
+        // Reset the form (number of cards input)
+        document.getElementById("card-form").reset();
+
+        enterClicked = false;
+
+         document.getElementById('form-message').textContent = "";
+    });
+
 });
